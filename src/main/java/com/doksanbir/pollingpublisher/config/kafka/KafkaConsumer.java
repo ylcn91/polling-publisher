294c2530e68a7ecab1b3a6c735f06c7215dd75c0
doksanbir <yalcindoksanbir@gmail.com>
2023-12-17 14:45:49 +0300

    Added consumer and producer support

--- src/main/java/com/doksanbir/pollingpublisher/config/kafka/KafkaConsumer.java
+++ src/main/java/com/doksanbir/pollingpublisher/config/kafka/KafkaConsumer.java
@@ -1,4 +1,24 @@
 package com.doksanbir.pollingpublisher.config.kafka;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class KafkaConsumer {
+
+
+    @KafkaListener(topics = "", groupId = "")
+    public void listen(String message) {
+        log.info("Received message: {}", message);
+        try {
+           log.info("Processing message: {}", message);
+        } catch (Exception e) {
+            log.error("Error processing message: {}", message, e);
+            // Handle the processing exception
+        }
+    }
 }
--- src/main/java/com/doksanbir/pollingpublisher/config/kafka/KafkaProducer.java
+++ src/main/java/com/doksanbir/pollingpublisher/config/kafka/KafkaProducer.java
@@ -1,4 +1,36 @@
 package com.doksanbir.pollingpublisher.config.kafka;
 
-    public class KafkaProducer {
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class KafkaProducer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String topic, String message) {
+        CompletableFuture<SendResult<String, String>> future = CompletableFuture.supplyAsync(() -> {
+            try {
+                return kafkaTemplate.send(topic, message).get();
+            } catch (InterruptedException | ExecutionException e) {
+                log.error("Failed to send message={} to topic={}", message, topic, e);
+                throw new RuntimeException(e);
+            }
+        });
+
+        try {
+            SendResult<String, String> result = future.get(); // Wait for the result
+            log.info("Sent message={} to topic={} with offset={}", message, topic, result.getRecordMetadata().offset());
+        } catch (InterruptedException | ExecutionException e) {
+            log.error("Failed to send message={} to topic={}", message, topic, e);
+        }
     }
+}
