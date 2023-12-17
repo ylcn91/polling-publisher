969508d30fa63f500ba3e956daca1548250e9dab
doksanbir <yalcindoksanbir@gmail.com>
2023-12-17 11:43:30 +0300

    minor changes on KafkaOutboxScheduler

--- src/main/java/com/doksanbir/pollingpublisher/config/kafka/KafkaOutboxScheduler.java
+++ src/main/java/com/doksanbir/pollingpublisher/config/kafka/KafkaOutboxScheduler.java
@@ -3,25 +3,29 @@ package com.doksanbir.pollingpublisher.config.kafka;
 import com.doksanbir.pollingpublisher.model.OutboxMessage;
 import com.doksanbir.pollingpublisher.service.OutboxMessageService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class KafkaOutboxScheduler {
 
     private final OutboxMessageService outboxMessageService;
     private final KafkaTemplate<String, String> kafkaTemplate;
 
-    @Scheduled(fixedRate = 10000)
+    @Scheduled(fixedDelay = 10000)
     public void publishUnsentMessages() {
         List<OutboxMessage> unsentMessages = outboxMessageService.findByStatus(OutboxMessage.MessageStatus.UNSENT);
-        for (OutboxMessage message : unsentMessages) {
+        unsentMessages.parallelStream().forEach(message -> {
+            log.info("Publishing message with ID {}, topic: {}, payload: {}", message.getId(), message.getTopic(), message.getPayload());
             kafkaTemplate.send(message.getTopic(), message.getPayload());
             message.setStatus(OutboxMessage.MessageStatus.SENT);
             outboxMessageService.saveOrUpdate(message);
-        }
+        });
     }
 }
